3d7377a6e48b03fa42a6d363eade33fd3b1de168
evan <evan@evanw.org>
2023-09-06 18:05:43 -0500

    slightly unbroke things

--- shkype.sh
+++ shkype.sh
@@ -1,83 +1,90 @@
 #!/bin/bash
 
 TITLE=Shkype
-PORT_STATUS=8000
-PORT_VIDEO=8001
+CONTACTS_FILE=/tmp/contacts.txt
+
+if [ -f ${PORT_STATUS} ]
+then
+    PORT_STATUS=8123
+fi
+if [ -f ${PORT_VIDEO} ]
+then
+    PORT_VIDEO=8124
+fi
 
 selected_contact=""
-pending_request=/tmp/pending_request
-rm $pending_request
+sender=/tmp/sender
+rm -f $sender
 
 interface=$(ip route|awk '/default/{print $5;exit}')
 ip_addr=$(ip addr show $interface|awk '/inet/{split($2, a, "/");print a[1];exit}')
 
-function foo() {
+function log() {
     echo "$1" >> /tmp/log
 }
 
-foo "--------------------------"
+log "--------------------------"
 
 
 function contacts() {
     # get whiptail output
     # https://stackoverflow.com/questions/1970180/whiptail-how-to-redirect-output-to-environment-variable
-    selected_contact=$(
+    readarray -t contacts < ${CONTACTS_FILE}
+    selected_address=$(
         whiptail \
             --title "$TITLE" \
             --menu "Choose a contact to call" 30 50 16 \
-            "Bobby" "" \
-            "Hank" "" \
-            "Peggy" "" \
-            "Luanne" "" \
-            "Dale" "" 3>&1 1>&2 2>&3
+            ${contacts[@]} \
+            3>&1 1>&2 2>&3
     )
     if (( $? == 0 ))
     then
         # FIXME - global variable hack for contacts subshell
-        touch $pending_request
-        foo "selected_contact: $selected_contact"
-        # request
-        nc 10.192.38.171 $PORT_STATUS <<< $ip_addr
+        touch $sender
+        nc -N ${selected_address} $PORT_STATUS <<< $ip_addr
+        log "sent my address ${ip_addr} to ${selected_address}"
     fi
 }
 
 while true
 do
-    foo "calling contacts"
+    log "displaying contacts"
     contacts &
 
     # accept/request
-    foo "waiting request"
-    incoming_call=$(nc -l $PORT_STATUS)
+    log "waiting request"
+    incoming_call=$(nc -l -p $PORT_STATUS)
 
-    foo "pending_request: $pending_request"
+    log "pending_request: ${incoming_call}"
 
     # kill contacts subshell
     killall whiptail
 
-    if [ -f $pending_request ]
-    then # accept
-        foo "selected_contact"
-
-        (mpv /dev/video0 --fps=15 --vo=tct 2>/dev/null| nc $incoming_call $PORT_VIDEO) &
+    if [ -f $sender ]
+    then
+        # we're the sender.  send video and give them our address
+        log "selected_contact"
 
         nc -l -p $PORT_VIDEO &
-        nc $incoming_call $PORT_STATUS <<< $ip_addr
+        (mpv /dev/video0 --fps=15 --vo=tct 2>/dev/null| nc $incoming_call $PORT_VIDEO) &
+
+        # nc -N $incoming_call $PORT_STATUS <<< $ip_addr
 
         # FIXME - max 2 minute call
         sleep 120
         killall nc
         killall mpv
 
-    else # request (receiver)
-        foo "incoming call"
+    else
+        # we're the receiver
+        log "incoming call"
         whiptail --title "$TITLE" --yesno "Incoming call from $incoming_call. Accept?" 8 78
         # FIXME
         if true
         then
+            nc -N $incoming_call $PORT_STATUS <<< $ip_addr
             nc -l -p $PORT_VIDEO &
-            (nc -l -p $PORT_STATUS; mpv /dev/video0 --fps=15 --vo=tct 2>/dev/null | nc $incoming_call $PORT_VIDEO) &
-            nc $incoming_call $PORT_STATUS <<< $ip_addr
+            (mpv /dev/video0 --fps=15 --vo=tct 2>/dev/null | nc $incoming_call $PORT_VIDEO) &
 
             # FIXME - max 2 minute call
             sleep 120
@@ -87,7 +94,7 @@ do
             echo foop
         fi
 
-        foo "accepted/rejected call"
+        log "accepted/rejected call"
     fi
 
 done
